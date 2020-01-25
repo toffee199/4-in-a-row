7fe307b19fcf0b543105d1059c96e93e11563785
Chen Elisha <han.elisha@gmail.com>
2020-01-25 21:11:28 +0200

    added mouse events

--- Assets/Scripts/Views/ColumnView.cs
+++ Assets/Scripts/Views/ColumnView.cs
@@ -1,10 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ColumnView : BaseView
+public class ColumnView : BaseView, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public SlotView[] slots;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //SlotState playerColor = GameController.gameInstance.GetCurrentSlotState();
+        slots[slots.Length - 1].SetState(SlotState.RED);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        slots[slots.Length - 1].SetState(SlotState.EMPTY);
+    }
 
     public void Reset()
     {
